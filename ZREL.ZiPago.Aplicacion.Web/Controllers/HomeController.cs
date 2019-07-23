81080e47b5ffdae21db2e55f024035f833f74a55
Luis Meneses <lmeneses@zigleet.com>
2019-07-22 19:36:59 -0500

    22.07.2019

--- ZREL.ZiPago.Aplicacion.Web/Controllers/HomeController.cs
+++ ZREL.ZiPago.Aplicacion.Web/Controllers/HomeController.cs
@@ -11,11 +11,6 @@ namespace ZREL.ZiPago.Aplicacion.Web.Controllers
             return View("~/Views/Home/Index.cshtml");
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
