1ec1fa2faed2c98a53d3dc957126e826a3618d7d
u201301059 (Meneses Medina, Luis Robinson) <u201301059@upc.edu.pe>
2020-03-09 08:49:31 -0500

    08.03.20

--- ZREL.ZiPago.Aplicacion.Web/Models/Afiliacion/ComercioViewModel.cs
+++ ZREL.ZiPago.Aplicacion.Web/Models/Afiliacion/ComercioViewModel.cs
@@ -9,9 +9,14 @@ namespace ZREL.ZiPago.Aplicacion.Web.Models.Afiliacion
     public class ComercioViewModel
     {
         public int IdUsuarioZiPago { get; set; }
-
-        public string Clave1 { get; set; }
-
+        public int IdComercioZiPagoReg { get; set; }
+        public string CodigoComercio { get; set; }
+        public string Descripcion { get; set; }
+        public string CorreoNotificacion { get; set; }
+        public string Estado { get; set; }
+        public string Activo { get; set; }
+        public int CodigoCuenta { get; set; }
+        //public string Clave1 { get; set; }
         public List<BancoZiPago> Bancos { get; set; }
         
     }
